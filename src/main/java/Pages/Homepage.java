149827bdcedfb4b52cb458cb2bcf57080b6ebcaa
snehamahat211 <snehamahat211@gmail.com>
2025-11-04 12:24:01 +0545

    basetests for orangehrm completed

--- src/main/java/Pages/Homepage.java
+++ src/main/java/Pages/Homepage.java
@@ -2,8 +2,36 @@ package Pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class Homepage {
+    public WebDriver driver;
+    private By userfield = By.name("username");
+    private By passwordfield = By.name("password");
+    private By loginbutton = By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button");
+    public Homepage(WebDriver driver) {
+
+        this.driver = driver;
+    }
+
+    public void setUsername(String username1) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(userfield)).sendKeys(username1);
+    }
+    public void setPassword(String password1) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(passwordfield)).sendKeys(password1);
+    }
+
+    public void  clickLoginButton()
+    {
+        driver.findElement(loginbutton).click();
+    }
+
 
 
 
