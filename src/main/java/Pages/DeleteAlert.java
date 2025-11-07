95ce81d64eaad8c0a25ef4f7a53e7caf2194ef54
snehamahat211 <snehamahat211@gmail.com>
2025-11-07 09:10:27 +0545

    work on alert messages

--- src/main/java/Pages/DeleteAlert.java
+++ src/main/java/Pages/DeleteAlert.java
@@ -1,10 +1,18 @@
 package Pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class DeleteAlert {
     private WebDriver driver;
+    private By alertDelete = By.xpath("//*[@id=\"app\"]/div[3]/div/div/div/div[3]/button[2]");
     public DeleteAlert(WebDriver driver) {
         this.driver = driver;
+    }
+    public void clickDelete() {
+        driver.findElement(alertDelete).click();
+    }
+    public void acceptAlert() {
+        driver.switchTo().alert().accept();
     }
 }
