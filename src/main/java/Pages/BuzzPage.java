580a1e049a7c33163ad23a2fc6f370f35e2511e5
snehamahat211 <snehamahat211@gmail.com>
2025-11-18 12:40:05 +0545

    buzztests locator

--- src/main/java/Pages/BuzzPage.java
+++ src/main/java/Pages/BuzzPage.java
@@ -1,4 +1,24 @@
 package Pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public class BuzzPage {
+    public WebDriver driver;
+    private WebDriverWait wait;
+    private By buzz = By.xpath("//*[@id=\"app\"]/div[1]/div[1]/aside/nav/div[2]/ul/li[12]/a/span");
+
+    public BuzzPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void BuzzPage() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(buzz)).click();
+    }
 }
+
--- src/test/java/buzz/buzztest.java
+++ src/test/java/buzz/buzztest.java
@@ -1,4 +1,14 @@
 package buzz;
 
-public class buzztest {
+import Base.BaseTests;
+import org.testng.annotations.Test;
+
+public class buzztest extends BaseTests {
+    @Test
+    public void buzztest()
+    {
+        homepage.setUsername("Admin");
+        homepage.setPassword("admin123");
+
+    }
 }
